cf78df47ca3bb0e977608e846cc6082adf51c77e
Alexander deCurnou <adecurnou@gmail.com>
2017-06-16 23:23:52 -0400

    Changed server backend to Flash, implemented basic youtube download

--- server.py
+++ server.py
@@ -1,27 +1,62 @@
-import json
+from flask import Flask, flash, render_template, request
+from flask_uploads import patch_request_class
+from flask_uploads import UploadSet, AUDIO, UploadConfiguration
+import logging
 import os
 import time
-import tornado
-import tornado.ioloop
-import tornado.web
+import youtube_dl as ytdl
 
-WEB_STUFF = os.path.join(os.path.dirname(__file__), 'web_stuff')
-UPLOAD_DIR = os.path.join(os.path.dirname(__file__), 'uploads')
+UPLOADS_DEFAULT_DEST = os.path.join(os.path.dirname(__file__), 'uploads')
+YT_DOWNLOADS = os.path.join(os.path.dirname(__file__), 'yt_downloads')
 
-class MainHandler(tornado.web.RequestHandler):
-	"""Renders the landing page"""
-	def get(self):
-		self.render(os.path.join(WEB_STUFF, 'index.html'))
 
-class UploadHandler(tornado.web.RequestHandler):
-	def post(self):
-		# Fill me in
+# youtube-dl spits out a metric ton of information, so we define a logging
+# object to handle all the spam
+class AppLogger(object):
+    def debug(self, msg):
+        pass
 
-if __name__ == "__main__":
-	app = tornado.web.Application([
-		(r'/', MainHandler),
-		(r'/web_stuff/(.*)', tornado.web.StaticFileHandler, {'path': WEB_STUFF}),
-		(r'/uploads/(.*)', tornado.web.StaticFileHandler, {'path': UPLOAD_DIR})
-		])
-	app.listen(9999)
-	tornado.ioloop.IOLoop.current().start()
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+yt_opts = {
+    # v0: high ratio of quality:size, and won't affect the spectrogram greatly
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '256',
+    }]
+}
+
+app = Flask(__name__)
+app.config['UPLOADS_DEFAULT_DEST'] = UPLOADS_DEFAULT_DEST
+songs = UploadSet('songs', AUDIO)
+
+UploadConfiguration(app, songs)
+patch_request_class(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'audio' in request.files:
+        f = songs.save(request.files['song'])
+
+
+@app.route('/youtube', methods=['GET', 'POST'])
+def youtube():
+    if request.method == 'POST':
+        song_link = request.form['link']
+        print(song_link)
+        with ytdl.YoutubeDL(yt_opts) as ydl:
+            ydl.download([song_link])
+        return render_template('index.html')
