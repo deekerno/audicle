35ad005f13f4a973221b0937dc36a5a26e7808e1
Szlosek <c.szlosek@ubreakifix.com>
2017-07-21 17:30:26 -0400

    These didn't get in there for some reason.

--- Visualizer-Web-App/src/app/file-upload.component.ts
+++ Visualizer-Web-App/src/app/file-upload.component.ts
@@ -20,18 +20,19 @@ export class FileUploadComponent {
     }
 
     onChange(event) {
-        console.log('fired');
         let files: FileList =  event.srcElement.files;
         this.file = files[0];
         this.song = this.sanitize(URL.createObjectURL(this.file));
+        this.sendFile(this.song);
     }
 
     sanitize(url:string){
         return this.sanitizer.bypassSecurityTrustUrl(url);
     }
 
-    sendFile() {
-        this.UploadService.uploadFile(this.song).subscribe((data) => {
+    sendFile(song) {
+        console.log('fired');
+        this.UploadService.uploadFile(song).subscribe((data) => {
             this.router.navigate(['/'])
         });
     }
