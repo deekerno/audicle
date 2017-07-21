2ea0034d95a1d9e2e45659fd06e747613184c420
Alexander deCurnou <adecurnou@gmail.com>
2017-07-21 01:52:39 -0400

    Created dataset compiler and neural network trainer; including model weights as well

---
+++ sound.py
@@ -0,0 +1,52 @@
+import librosa as lbr
+import numpy as np
+import ffmpy
+
+WINDOW_SIZE = 2048
+# Stride controls how the window convolves around the input
+WINDOW_STRIDE = WINDOW_SIZE // 2 # integer division
+N_MELS = 128
+MEL_KWARGS = {
+    'n_fft': WINDOW_SIZE,
+    'hop_length': WINDOW_STRIDE,
+    'n_mels': N_MELS
+}
+
+class Sound(object):
+    """
+    Any time a song is passed to the system, we'll make an object so that
+    we can easily access its corresponding characteristics. The numerical
+    attributes are being filled with None values at instantiation so that
+    we can test if audio processing framework is actually setting the values
+    of the object.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        # librosa_rep is the time series given after loading a file with librosa
+        self.librosa_rep = None
+        self.samp_rate = None
+        self.duration = None 
+        self.tempo = None
+        self.spectro = None
+        # spectro_human is the human-readable version in case we want to present it
+        self.spectro_human = None
+
+def load_and_gen_obj(self):
+    self.librosa_rep, self.samp_rate = lbr.load(self.filename)
+    # ".T" gives the transposed version of the NumPy array
+    self.spectro = lbr.feature.melspectrogram(self.librosa_rep, self.samp_rate, **MEL_KWARGS).T
+
+def load_and_gen(filename, given_shape=None):
+    librosa_rep, samp_rate = lbr.load(filename, mono=True)
+    mel_spec = lbr.feature.melspectrogram(librosa_rep, **MEL_KWARGS).T
+
+    if given_shape is not None:
+        if mel_spec.shape[0] < given_shape[0]:
+            delta_shape = (given_shape[0] - mel_spec.shape[0],
+                    given_shape[1])
+            mel_spec = np.append(mel_spec, np.zeros(delta_shape), axis=0)
+        elif mel_spec.shape[0] > given_shape[0]:
+            mel_spec = mel_spec[: given_shape[0], :]
+
+    mel_spec[mel_spec == 0] = 1e-6
+    return (np.log(mel_spec), float(librosa_rep.shape[0]) / samp_rate)
--- trainer.py
+++ trainer.py
@@ -1,10 +1,14 @@
 from keras import backend as K
+from keras.callbacks import Callback
+from keras.utils import np_utils
+from keras.models import Model
 from keras.layers import (
 	Activation,
-	Conv1D,
+	Convolution1D,
 	Dense,
 	Dropout,
 	Input,
+	Lambda,
 	LSTM,
 	MaxPooling1D,
 	TimeDistributed,
@@ -14,7 +18,71 @@ from keras.utils import np_utils
 from sklearn.model_selection import train_test_split as tts
 import numpy as np
 import os
+import pickle
 
 
 SEED = 4331    # random seed for the network
-NUM_LAYERS = 3
+NUM_LAYERS = 3 # number of conv-act-pool layers before dropout
+NUM_GENRES = 8
+FILTER_LENGTH = 5
+CONV_FILTER_COUNT = 256
+LSTM_COUNT = 256
+BATCH_SIZE = 32
+EPOCH_COUNT = 100
+
+def trainer(pickle):
+	x = pickle['x']
+	y = pickle['y']
+	(x_training, x_validation, y_training, y_validation) = tts(x, y, test_size=0.3, random_state=SEED)
+
+	print("Constructing Neural Network...")
+	num_features = x_training.shape[2]
+	input_shape = (None, num_features)
+	model_input = Input(input_shape, name='input')
+	layer = model_input
+
+	for i in range(NUM_LAYERS):
+		layer = Convolution1D(
+			CONV_FILTER_COUNT,
+			FILTER_LENGTH,
+			)(layer)
+		layer = Activation('relu')(layer)
+		layer = MaxPooling1D(2)(layer)
+
+	layer = Dropout(0.5)(layer)
+	layer = LSTM(LSTM_COUNT, return_sequences=True)(layer)
+	layer = Dropout(0.5)(layer)
+	layer = TimeDistributed(Dense(NUM_GENRES))(layer)
+	layer = Activation('softmax', name='output_realtime')(layer)
+
+	# Gonna be real, this is black magic from Github and Stack Overflow.
+	time_dist_merge = Lambda(
+		function=lambda x: K.mean(x, axis=1), 
+		output_shape=lambda shape: (shape[0],) + shape[2:],
+		name='output_merged'
+		)
+
+	model_output = time_dist_merge(layer)
+	model = Model(model_input, model_output)
+	opt = Adam(lr=0.00001)
+	model.compile(
+		loss='categorical_crossentropy',
+		optimizer=opt,
+		metrics=['accuracy']
+		)
+	print("Neural Network Constructed.")
+
+	print("Training...")
+	model.fit(x_training, y_training, batch_size=BATCH_SIZE, epochs=EPOCH_COUNT,
+			validation_data=(x_validation, y_validation), verbose=1)
+
+	return model
+
+data = pickle.load(open('data.pkl', 'rb'))
+
+model = trainer(data)
+
+with open('model.yaml', 'w') as f:
+	f.write(model.to_yaml())
+
+model.save_weights('weights.h5')
