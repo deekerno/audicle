5fe89467a78ee7199ca2983f43d9bd7132c7f4b9
Szlosek <c.szlosek@ubreakifix.com>
2017-07-21 19:16:00 -0400

    static is working and up to date.

---
+++ static/src/app/audio-player.service.ts
@@ -0,0 +1,10 @@
+import { Injectable} from '@angular/core';
+
+@Injectable()
+export class AudioPlayerService {
+
+    userAudio;
+
+    constructor() { }
+
+}
---
+++ static/src/app/visualizer.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'visualizer',
+  templateUrl: './visualizer.component.html'
+})
+
+export class VisualizerComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    
+  }
+
+}
